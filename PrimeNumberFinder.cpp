af578e7f522bdba9903c0b3dd917fb1a1942be5f
sloppyolivermon9 <143131749+sloppyolivermon9@users.noreply.github.com>
2025-10-27 00:35:59 +0000

    Update PrimeNumberFinder.cpp

--- PrimeNumberFinder.cpp
+++ PrimeNumberFinder.cpp
@@ -1,4 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <cmath>
 
+using std::cout;
+using std::string;
+using std::endl;
+
+using std::ofstream;
+using std::ifstream;
+using std::ios_base;
+using std::to_string;
+
+//Global Variables
+unsigned long long int primesFound[100000];
+unsigned long long int currentNum;
+unsigned int currentPrimes = 0;
+
+bool isPrime(int num) {
+    if (num < 2) {
+        return false; // Numbers less than 2 are not prime
+    }
+
+    // Check divisibility from 2 up to the square root of num
+    for (unsigned long long int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            return false; // Found a factor, not prime
+        }
+    }
+
+    return true; // No factors found, prime
+}
+
+int getLastNum() { //gets number from .txt so it can resume progress
+    const string file = "currentNum.txt";
+    string output = "";
+    ifstream f(file);
+
+    if (!f.is_open()) {
+        ofstream f(file);
+        f << "2";
+        f.close();
+        ifstream f2(file);
+        getline(f2, output);
         f2.close();
         return stoi(output);
     }
